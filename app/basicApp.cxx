69af2e0f80609fd8468f71e9b2a8daebd946dd1d
Yan-Jie Schnellbach <7926955+yschnellbach@users.noreply.github.com>
2019-10-07 16:55:51 +0100

    Added basic test files

---
+++ app/basicApp.cxx
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<basicLib.h>
+
+int main(){
+
+  helloWorld();
+  return 0;
+
+}
---
+++ src/basicLib.cxx
@@ -0,0 +1,5 @@
+#include<basicLib.h>
+
+void helloWorld(){
+  std::cout << "Hello World!" << std::endl;
+}
